c87164d293330d2d6b91300cf629a941df1fdd55
Umschaden Michael <Umschaden.Michael@gmx.at>
2018-03-09 23:11:38 +0100

    Fertiges Projekt

--- app/src/main/java/com/example/michael/palindromchecker/MainActivity.java
+++ app/src/main/java/com/example/michael/palindromchecker/MainActivity.java
@@ -10,8 +10,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
 
-    //Das Projekt ist noch leer und bis auf das Layout wurde boch nichts gemacht.
-   
+    //Projekt ist fertig implementiert und funktioniert -> Bitte Danke ich haben fertig XD.
 
 
     TextView output;
@@ -33,9 +32,38 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view){
                 String palindrom = eingabefeld.getText().toString();
-                output.setText(palindrom);
+                //Bedingungen fuer die Eingaben
+                if(palindrom.length()< 5){
+                    String tooShort = "Wort muss mindestens 5 Buchstaben haben";
+                    output.setText(tooShort);
+                }else if(palindrom == null){
+                    String noinput = "Sie haben nichts eingeben, bitte geben sie eine Wort ein";
+                    output.setText(noinput);
+                }else if(is_Palindrom(palindrom) == true){
+                    String ispali = "Word is a palindrom";
+                    output.setText(ispali);
+                }else if(is_Palindrom(palindrom) == false){
+                    String isnot = "Word is not a palindrom";
+                    output.setText(isnot);
+                }
             }
         });
 
+    }
+
+    //Checkt ob die Eingabe ein Palindrom ist
+    public boolean is_Palindrom(String palindrom){
+
+        boolean checker = true;
+        String formated = palindrom.toLowerCase();
+        char[] arr = new char[palindrom.length()];
+        arr = formated.toCharArray();
+
+        for(int i = 0;i<arr.length/2;i++){
+            if(arr[i] != arr[arr.length-1-i]){
+                checker = false;
+            }
+        }
+        return checker;
     }
 }
